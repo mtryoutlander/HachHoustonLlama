433e3c662861e58c3748bed2f67fcd5d48686776
Jaiveon <Jaiveon@10.30.73.203>
2019-04-12 20:13:13 -0500

    hi

--- src/CrossCheck.java
+++ src/CrossCheck.java
@@ -4,9 +4,9 @@ public class CrossCheck
 {
 	public boolean Cross(ArrayList<Nodes> a)
 	{
-		for(int i=0;i<a.length;i++)
+		for(int i=0;i<a.size();i++)
 		{
-			for(int j=0;j<a.length;j++)
+			for(int j=0;j<a.size();j++)
 			{
 				if(a.get(j).equals(a.get(i)))
 				{
