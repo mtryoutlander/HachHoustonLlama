2587db097b9800048bf488a2e408c52b0eb4b90c
Ronnie <Ronnie@10.30.42.129>
2019-04-12 20:09:46 -0500

    e

--- src/e.java
+++ src/e.java
@@ -13,10 +13,10 @@ public class e {
 
 		Nodes nodes1 = new Nodes(1, 5);
 
-		Nodes[] array = new Nodes[inputs.length() - 1];
+		Nodes[] array = new Nodes[inputs.length()];
 		array[0] = nodes1;
 
-		for (int i = 1; i < inputs.length(); i++) {
+		for (int i = 0; i < inputs.length(); i++) {
 			if (inputs.charAt(i) == 'R')
 				x++;
 			if (inputs.charAt(i) == 'L')
@@ -29,11 +29,15 @@ public class e {
 				System.out.println("Only R,L,U, and D are acceptable characters");
 
 			Nodes nodes2 = new Nodes(x, y);
-			array[i] = nodes2;
+			array[i + 1] = nodes2;
 		}
-		
-		if(array[inputs.length() - 1] == new Nodes(5, 1))
-			System.out.println("The line is legal");
+
+		if (array[inputs.length()] == new Nodes(5, 1))
+			System.out.println("The line ends at the correct node");
+		else
+			System.out.println("Line does not end at the correct node");
+
+		ArrayList<Nodes> list = new ArrayList<>(Arrays.asList(array));
 	}
 
 }
